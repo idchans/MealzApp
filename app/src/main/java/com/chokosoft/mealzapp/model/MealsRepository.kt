88913c44192f7c8a9eb4607007b637414d6f1180
Chander Siddarth <aulfster@gmail.com>
2024-03-01 18:09:15 -0800

    Add the model layer. And the gson library to map the json to the data class.

--- app/src/main/java/com/chokosoft/mealzapp/model/MealsRepository.kt
+++ app/src/main/java/com/chokosoft/mealzapp/model/MealsRepository.kt
@@ -1,5 +1,9 @@
 package com.chokosoft.mealzapp.model
 
+import com.chokosoft.mealzapp.model.response.MealsCategoriesResponse
+
 // Gets and maps the data from the server.
 class MealsRepository {
+    fun getMeals(): MealsCategoriesResponse = MealsCategoriesResponse(arrayListOf())
+
 }
--- app/src/main/java/com/chokosoft/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/chokosoft/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -2,6 +2,10 @@ package com.chokosoft.mealzapp.ui.meals
 
 import androidx.lifecycle.ViewModel
 import com.chokosoft.mealzapp.model.MealsRepository
+import com.chokosoft.mealzapp.model.response.MealResponse
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()): ViewModel() {
+    fun getMeals(): List<MealResponse>{
+        return repository.getMeals().categories
+    }
 }
