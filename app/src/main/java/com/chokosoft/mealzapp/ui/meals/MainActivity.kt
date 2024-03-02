1a0f84f975c7d5b9ab28bceda00da02e90b8b4eb
Chander Siddarth <aulfster@gmail.com>
2024-03-01 18:53:05 -0800

    Added a lot of callbacks and ugly javascript like stuff to make retrofit get the data.

--- app/src/main/java/com/chokosoft/mealzapp/ui/meals/MainActivity.kt
+++ app/src/main/java/com/chokosoft/mealzapp/ui/meals/MainActivity.kt
@@ -3,10 +3,16 @@ package com.chokosoft.mealzapp.ui.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.chokosoft.mealzapp.model.response.MealResponse
 import com.chokosoft.mealzapp.ui.theme.MealzAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +29,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel: MealsCategoriesViewModel = viewModel()
-    val meals = viewModel.getMeals()
-    Text(
-        text = meals.toString()
-    )
+    val rememberedMeals: MutableState<List<MealResponse>> = remember { mutableStateOf(emptyList<MealResponse>())
+
+    }
+    viewModel.getMeals { response ->
+        val meals = response?.categories
+        rememberedMeals.value = meals.orEmpty()
+    }
+    LazyColumn {
+        items(rememberedMeals.value) { meals->
+            Text(
+                text = meals.name
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/chokosoft/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/chokosoft/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -3,9 +3,13 @@ package com.chokosoft.mealzapp.ui.meals
 import androidx.lifecycle.ViewModel
 import com.chokosoft.mealzapp.model.MealsRepository
 import com.chokosoft.mealzapp.model.response.MealResponse
+import com.chokosoft.mealzapp.model.response.MealsCategoriesResponse
+import retrofit2.Callback
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()): ViewModel() {
-    fun getMeals(): List<MealResponse>{
-        return repository.getMeals()?.categories.orEmpty()
+    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
+        repository.getMeals() { response ->
+            successCallback(response)
+        }
     }
 }
